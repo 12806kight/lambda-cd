87b8b725314fdea576b06d4f47907bbf7983e615
Brewer <12806kight@gmail.com>
2022-12-04 22:28:10 -0800

    Try 2

--- function.py
+++ function.py
@@ -2,8 +2,9 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    #Try 2
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-    "Hello World."
+    
